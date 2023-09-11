3dcb041f7644178e70e33d230d18f0779bc60c29
Densu341 <denyirawan341@gmail.com>
2023-09-11 23:15:13 +0700

    new ui tictactoe

--- tictactoe-react/src/main.jsx
+++ tictactoe-react/src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
