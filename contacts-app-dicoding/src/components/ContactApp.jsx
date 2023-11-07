4907a285c471de6bfe9fec9c208418a4e03e73b6
Densu341 <denyirawan341@gmail.com>
2023-11-07 22:07:13 +0700

    add new study project dicoding contact app react

---
+++ contacts-app-dicoding/src/components/ContactApp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "../utils/data";
+
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: getData(),
+    };
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  onDeleteHandler(id) {
+    const contacts = this.state.contacts.filter((contact) => contact.id !== id);
+    this.setState({ contacts });
+  }
+
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList
+          contacts={this.state.contacts}
+          onDelete={this.onDeleteHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default ContactApp;
