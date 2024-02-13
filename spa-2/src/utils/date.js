fefa5b3aa78bfe45b707d0bf75f159e7564e6c0b
DENI IRAWAN <87279587+Densu341@users.noreply.github.com>
2024-02-14 02:18:31 +0700

    Delete spa-2 directory

--- spa-2/src/utils/date.js
+++
@@ -1,9 +0,0 @@
-export default function showFormattedDate(date) {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return new Date(date).toLocaleDateString("id-ID", options);
-}
